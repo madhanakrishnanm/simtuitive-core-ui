1588425b3f1d8282b15aaf93a3cd64c82e3c1008
Karthick M <mkarthick334.mk@gmail.com>
2020-04-28 22:30:06 +0530

    Modal Changes

--- src/app/pages/admins/admins.component.ts
+++ src/app/pages/admins/admins.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 import {FormBuilder} from '@angular/forms';
 import {AdminService} from '../../services/admin.service';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+
 @Component({
   selector: 'app-admins',
   templateUrl: './admins.component.html',
@@ -12,13 +13,16 @@ export class AdminsComponent implements OnInit {
   closeResult = '';
   admins = [];
   deleteUserId = null;
+
   constructor(public router: Router,
               private formBuilder: FormBuilder,
               private ngModalService: NgbModal,
-              private adminService: AdminService,private modalService: NgbModal) { }
-              open(content) {
-                this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'});
-              }
+              private adminService: AdminService, private modalService: NgbModal) {
+  }
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'});
+  }
 
 
   ngOnInit(): void {
@@ -28,20 +32,22 @@ export class AdminsComponent implements OnInit {
       this.admins = res.data;
     });
   }
-  requestDelete(userId, modalReference){
+
+  requestDelete(userId, modalReference) {
     this.deleteUserId = userId;
-    this.modalService.open(modalReference)
+    this.modalService.open(modalReference, {centered: true, size: 'sm', windowClass: 'simtuitive-modal'});
 
   }
-  delete(){
-    let payload = {
-      userId : this.deleteUserId
+
+  delete() {
+    const payload = {
+      userId: this.deleteUserId
     };
     console.log(payload);
-    this.adminService.deleteAdmin(payload).subscribe((res: any)=>{
+    this.adminService.deleteAdmin(payload).subscribe((res: any) => {
       console.log(res);
       window.location.reload();
-    })
+    });
   }
 
 }
