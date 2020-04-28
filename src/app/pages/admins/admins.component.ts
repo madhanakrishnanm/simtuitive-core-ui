c11917deea9288ae777c64f8240eca0084d9754f
Mani Maran <mani@wisdomtoolz.com>
2020-04-28 18:24:49 +0530

    Modal For delete Confirmation

--- src/app/pages/admins/admins.component.ts
+++ src/app/pages/admins/admins.component.ts
@@ -2,18 +2,23 @@ import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import {FormBuilder} from '@angular/forms';
 import {AdminService} from '../../services/admin.service';
-
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-admins',
   templateUrl: './admins.component.html',
   styleUrls: ['./admins.component.scss']
 })
 export class AdminsComponent implements OnInit {
-
+  closeResult = '';
   admins = [];
   constructor(public router: Router,
               private formBuilder: FormBuilder,
-              private adminService: AdminService) { }
+              private adminService: AdminService,private modalService: NgbModal) { }
+          
+              open(content) {
+                this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'});
+              }
+          
 
   ngOnInit(): void {
     const payload = {};
--- src/app/pages/admins/admins.module.ts
+++ src/app/pages/admins/admins.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminsComponent } from './admins.component';
-
-
-
 @NgModule({
   declarations: [AdminsComponent],
   imports: [
