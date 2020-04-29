a86a651c0f3000e0e8d00276dad3d7eb9f0792ce
Mani Maran <mani@wisdomtoolz.com>
2020-04-29 19:23:21 +0530

    search bar,filter,pagination in role page completed

--- src/app/pages/roles/roles.module.ts
+++ src/app/pages/roles/roles.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RolesComponent } from './roles.component';
+import {NgSelectModule} from '@ng-select/ng-select';
 
 
 
 @NgModule({
   declarations: [RolesComponent],
-  imports: [
-    CommonModule
-  ]
+    imports: [
+        CommonModule,
+        NgSelectModule
+    ]
 })
 export class RolesModule { }
