e2f0d442dca01b1593c132d3ac848b20cc70b86d
Mani Maran <mani@wisdomtoolz.com>
2020-04-29 10:59:00 +0530

    for get a DeleteOraganization

--- src/app/pages/organizations/organizations.component.ts
+++ src/app/pages/organizations/organizations.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import {FormBuilder} from '@angular/forms';
 import {OrganizationService} from '../../services/organization.service';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-organizations',
@@ -9,11 +10,11 @@ import {OrganizationService} from '../../services/organization.service';
   styleUrls: ['./organizations.component.scss']
 })
 export class OrganizationsComponent implements OnInit {
-
+   deleteId = null;
   organizations = [];
   constructor(public router: Router,
               private formBuilder: FormBuilder,
-              private organizationService: OrganizationService) { }
+              private organizationService: OrganizationService, private modalService: NgbModal) { }
 
   ngOnInit(): void {
     const payload = {};
@@ -22,5 +23,20 @@ export class OrganizationsComponent implements OnInit {
       this.organizations = res.data;
     });
   }
+  requestDelete(userId, modalReference) {
+    this.deleteId = userId;
+    this.modalService.open(modalReference, {centered: true, size: 'sm', windowClass: 'simtuitive-modal'});
 
+  }
+
+  delete() {
+    const payload = {
+      userId: this.deleteId
+    };
+    console.log(payload);
+    /*this.organizationService.(payload).subscribe((res: any) => {
+      console.log(res);
+      window.location.reload();
+    });*/
+  }
 }
