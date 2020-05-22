4f6310ba24fc4ecbeaed64d3b97da8fb39949551
Karthick <mkarthick334.mk@gmail.com>
2020-05-22 16:21:29 +0530

    View License

--- src/app/services/organization.service.ts
+++ src/app/services/organization.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {ApiService} from './api.service';
 
 @Injectable({
@@ -6,29 +6,37 @@ import {ApiService} from './api.service';
 })
 export class OrganizationService {
 
-  constructor(private apiService: ApiService) { }
+  constructor(private apiService: ApiService) {
+  }
 
   addOrganization(payload) {
     return this.apiService.post('org/add-org', payload);
   }
+
   editOrganization(payload) {
     return this.apiService.put('org/update-org', payload);
   }
+
   getAllOrganization(payload) {
     return this.apiService.get('org/getall-org', payload);
   }
+
   getOrganizationById(payload) {
     return this.apiService.post('org/get-org', payload);
   }
+
   deleteOrganization(payload) {
     return this.apiService.delete('org/delete-org', payload);
   }
+
   findOrganizationLocation(payload) {
     return this.apiService.get('org/getall-org-location', payload);
   }
+
   findOrganizationIndustry(payload) {
     return this.apiService.get('org/getall-org-industry', payload);
   }
+
   findOrganizationName(payload) {
     return this.apiService.get('org/getall-orgname', payload);
   }
