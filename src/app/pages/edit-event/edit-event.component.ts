d30936bb512886fe3445406d94b68dd90810dfd1
Karthick <mkarthick334.mk@gmail.com>
2020-05-29 18:32:44 +0530

    Edit Changes

--- src/app/pages/edit-event/edit-event.component.ts
+++ src/app/pages/edit-event/edit-event.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {getDateFromObject} from '../../lib';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {EventService} from '../../services/event.service';
+import {NgxSmartModalService} from 'ngx-smart-modal';
+import {Router} from '@angular/router';
 @Component({
   selector: 'app-edit-event',
   templateUrl: './edit-event.component.html',
@@ -7,9 +11,192 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditEventComponent implements OnInit {
 
-  constructor() { }
+  // for next step functionality
+  // tslint:disable-next-line:no-output-on-prefix
+  @Output() onStepNext: EventEmitter<any> = new EventEmitter();
+  nextTitle = 'Event Summary';
+  next;
+  tollGate = '';
+  clients: any = ['Microsoft', 'HP', 'IBM', 'InfoSys'];
+  products: any = ['Java', 'Python', 'C#'];
+  modules = [
+    {
+      moduleNumber: 1,
+      moduleName: 'Operational Excellence 101',
+    },
+    {
+      moduleNumber: 2,
+      moduleName: 'Operations Planning',
+    },
+    {
+      moduleNumber: 3,
+      moduleName: 'CPU of Operations',
+    },
+    {
+      moduleNumber: 4,
+      moduleName: 'Talent Management',
+    },
+    {
+      moduleNumber: 5,
+      moduleName: 'Talent Acquisition',
+    },
+    {
+      moduleNumber: 6,
+      moduleName: 'Key Performance Indicators',
+    },
+    {
+      moduleNumber: 7,
+      moduleName: 'Train for Skill',
+    },
+    {
+      moduleNumber: 8,
+      moduleName: 'Operational Excellence 101',
+    },
+    {
+      moduleNumber: 9,
+      moduleName: 'Operations Planning',
+    },
+    {
+      moduleNumber: 10,
+      moduleName: 'CPU of Operations',
+    },
+    {
+      moduleNumber: 11,
+      moduleName: 'Key Performance Indicators',
+    },
+    {
+      moduleNumber: 12,
+      moduleName: 'Talent Acquisition'
+    }
+  ];
+  eventForm: FormGroup;
+  sessions = [];
+  selectedModules = [];
+  addedModules = [];
+  editSession = 0;
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder,
+              private eventService: EventService,
+              public ngxSmartModalService: NgxSmartModalService,
+              private router: Router) {
+  }
+
+  get f() {
+    return this.eventForm.controls;
+  }
+
+  ngOnInit() {
+    this.eventForm = this.fb.group({
+      organizationName: [null, []],
+      clientName: [null, []],
+      productName: [null, []],
+      eventName: [null, []],
+      tollGates: ['', []],
+      noOfParticipants: ['', []],
+      eventStartDate: ['', []],
+      eventEndDate: ['', []],
+      notes: ['', []]
+    });
+  }
+
+  openEventCreate() {
+    this.ngxSmartModalService.open('addEvent');
+  }
+
+  save() {
+    console.log(this.eventForm.value);
+    const eventForm = this.eventForm.value;
+    eventForm.eventStartDate = getDateFromObject(eventForm.eventStartDate);
+    eventForm.eventEndDate = getDateFromObject(eventForm.eventEndDate);
+    eventForm.productName = 'Operational Excellence';
+    const sessions = [{
+      sessionName: 'Session 1',
+      date: '2020-05-29',
+      modules: [{
+        moduleNumber: 1,
+        moduleName: 'Operational Excellence 101'
+      },
+        {
+          moduleNumber: 2,
+          moduleName: 'Operations Planning'
+        },
+        {
+          moduleNumber: 3,
+          moduleName: 'CPU of Operations'
+        }]
+    },
+      {
+        sessionName: 'Session 2',
+        date: '2020-05-30',
+        modules: [{
+          moduleNumber: 4,
+          moduleName: 'Talent Management'
+        },
+          {
+            moduleNumber: 5,
+            moduleName: 'Talent Acquisition'
+          },
+          {
+            moduleNumber: 7,
+            moduleName: 'Key Performance Indicators'
+          }]
+      }];
+    const eventInfo = {
+      ...eventForm,
+      sessions
+    };
+    console.log(JSON.stringify(eventInfo));
+  }
+
+  setSelectedModule(module) {
+    if (this.selectedModules.indexOf(module) === -1) {
+      this.selectedModules.push(module);
+    } else {
+      const moduleIndex = this.selectedModules.indexOf(module);
+      this.selectedModules.splice(moduleIndex, 1);
+    }
+    console.log(module);
+  }
+
+  isChecked(module) {
+    return this.selectedModules.indexOf(module) !== -1;
+  }
+
+  addModules() {
+    this.modules.forEach((module, index) => {
+      this.selectedModules.forEach((selModule) => {
+        if (selModule.moduleNumber === module.moduleNumber) {
+          // @ts-ignore
+          module.isSelected = true;
+        }
+      });
+    });
+    this.addedModules = this.selectedModules;
+    console.log(this.addedModules);
+  }
+
+  removeModule(module) {
+    const moduleIndex = this.addedModules.indexOf(module);
+    this.addedModules.splice(moduleIndex, 1);
+    this.modules.forEach((mod, index) => {
+      if (mod.moduleNumber === module.moduleNumber) {
+        module.isSelected = false;
+      }
+    });
+  }
+
+  saveSession() {
+    const sessions = this.sessions;
+    if (this.editSession) {
+      sessions[this.editSession - 1] = {
+        sessionName: 'Session' + this.editSession
+      };
+    } else {
+    }
+    this.ngxSmartModalService.close('addEvent');
+  }
+  parseModules() {
+
   }
 
 }
